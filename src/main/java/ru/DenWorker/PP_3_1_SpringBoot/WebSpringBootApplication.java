76e8e51934eb5b45a68991d90d723305c0cc59ab
DenWorker <whiteskyD@yandex.ru>
2023-11-23 02:58:00 +0300

    Git check

--- src/main/java/ru/DenWorker/PP_3_1_SpringBoot/WebSpringBootApplication.java
+++ src/main/java/ru/DenWorker/PP_3_1_SpringBoot/WebSpringBootApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebSpringBootApplication {
 
+	// git check
 	//http://localhost:8080/users
 	public static void main(String[] args) {
 		SpringApplication.run(WebSpringBootApplication.class, args);
