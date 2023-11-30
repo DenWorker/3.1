8ca0c9b88e870e58f801b660f30407118ea243e8
DenWorker <whiteskyD@yandex.ru>
2023-11-30 10:34:44 +0300

    Fix from 3.1.2. (Security)

--- src/main/java/ru/DenWorker/PP_3_1_SpringBoot/controller/AuthController.java
+++ src/main/java/ru/DenWorker/PP_3_1_SpringBoot/controller/AuthController.java
@@ -1,33 +1,22 @@
 package ru.DenWorker.PP_3_1_SpringBoot.controller;
 
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.DenWorker.PP_3_1_SpringBoot.model.User;
 import ru.DenWorker.PP_3_1_SpringBoot.security.UserDetailsImpl;
-import ru.DenWorker.PP_3_1_SpringBoot.service.RoleService;
 import ru.DenWorker.PP_3_1_SpringBoot.service.UserDetailsServiceImpl;
-import ru.DenWorker.PP_3_1_SpringBoot.service.UserService;
 
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final UserService userService;
     private final UserDetailsServiceImpl userDetailsService;
-    private final RoleService roleService;
 
     @Autowired
-    public AuthController(UserService userService, UserDetailsServiceImpl userDetailsService, RoleService roleService) {
-        this.userService = userService;
+    public AuthController(UserDetailsServiceImpl userDetailsService) {
         this.userDetailsService = userDetailsService;
-        this.roleService = roleService;
     }
 
     @ModelAttribute("currentUser")
