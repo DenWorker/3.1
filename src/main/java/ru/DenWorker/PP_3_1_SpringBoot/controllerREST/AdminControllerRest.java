865c9b83b4cc72fffb4b2c36b7847957178cb11a
DenWorker <whiteskyD@yandex.ru>
2023-12-02 10:20:41 +0300

    Tasks from 3.3. (REST_API)

---
+++ src/main/java/ru/DenWorker/PP_3_1_SpringBoot/controllerREST/AdminControllerRest.java
@@ -0,0 +1,12 @@
+package ru.DenWorker.PP_3_1_SpringBoot.controllerREST;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminControllerRest {
+
+
+}
---
+++ src/main/resources/static/js/adminsScripts.js
@@ -0,0 +1,10 @@
+function showEditModal(userId) {
+    let editModal = new bootstrap.Modal(document.getElementById('editModal'));
+
+    editModal.show();
+}
+
+function showDeleteModal(userId) {
+   let editModal = new bootstrap.Modal(document.getElementById('deleteModal'));
+   editModal.show();
+}
